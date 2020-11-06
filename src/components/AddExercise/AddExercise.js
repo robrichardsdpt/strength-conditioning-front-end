56b8cfe3b0cdd8338fc1c81ef1421d4832508ad8
robrichardsdpt <robrichardsdpt@gmail.com>
2020-11-05 21:25:48 -0500

    exercises added successfully.
    Get all request working.  Populating on to the page will be next.  Workout view page will also follow.

--- src/components/AddExercise/AddExercise.js
+++ src/components/AddExercise/AddExercise.js
@@ -10,6 +10,7 @@ import messages from '../AutoDismissAlert/messages'
 class AddExercise extends React.Component {
   constructor (props) {
     super(props)
+    console.log(this.props)
     this.state = {
       workout: {
         client: '',
@@ -25,9 +26,10 @@ class AddExercise extends React.Component {
         rx_percentage: '',
         weight: '',
         notes: '',
-        owner: ''
+        workout: ''
       },
-      token: this.props.user.token
+      token: this.props.user.token,
+      createdExerciseId: ''
     }
   }
 
@@ -38,10 +40,11 @@ class AddExercise extends React.Component {
     // get the name of the input that the user typed in
     const exerciseKey = event.target.name
     // make a copy of the state
-    const exerciseCopy = Object.assign({}, this.state.run) // to get the original state of the run and to copy it into another object to bypass inability to assign to a state
+    const exerciseCopy = Object.assign({}, this.state.exercise) // to get the original state of the run and to copy it into another object to bypass inability to assign to a state
     // Object.assign({}, object-to-copy) allows you to combine two objects
     // updating the key in our state with what the user typed in
     exerciseCopy[exerciseKey] = userInput
+    exerciseCopy['workout'] = this.state.workout.id
     // updating the state with our new copy
     this.setState({ exercise: exerciseCopy
     })
@@ -62,7 +65,7 @@ class AddExercise extends React.Component {
       }
     })
       .then((response) => this.setState({
-        createdWorkoutId: response.data.workout.id
+        exerciseId: response.data.exercise.id
       })
       )
       .then(() => msgAlert({
@@ -91,25 +94,25 @@ class AddExercise extends React.Component {
             <Form onSubmit={this.handleSubmit} >
               <Form.Label><h5>Exercise:</h5></Form.Label>
               <Form.Control
-                name="name"
-                id="name"
-                type="text"
-                placeholder="Name of desired exercise"
+                name='name'
+                id='name'
+                type='text'
+                placeholder='Name of desired exercise'
                 onChange={this.handleChange}
               />
               <Form.Label><h5>Sets:</h5></Form.Label>
-              <Form.Control name="sets" id="sets" onChange={this.handleChange} type="number" placeholder="number of sets" min="0"/>
+              <Form.Control name='sets' id='sets' onChange={this.handleChange} type='number' placeholder='number of sets' min='0'/>
               <Form.Label><h5>Repetitions:</h5></Form.Label>
-              <Form.Control name="repetitions" id="repetitions" onChange={this.handleChange} type="number" placeholder="number of repetitions" min="0"/>
+              <Form.Control name='repetitions' id='repetitions' onChange={this.handleChange} type='number' placeholder='number of repetitions' min='0'/>
               <Form.Label><h5>Target rpe between 1-10 (optional):</h5></Form.Label>
-              <Form.Control name="rx_rpe" id="rx_rpe" onChange={this.handleChange} type="number" placeholder="rpe" min="0"/>
+              <Form.Control name='rx_rpe' id='rx_rpe' onChange={this.handleChange} type='number' placeholder='rpe' min='0'/>
               <Form.Label><h5>Target percentage (optional):</h5></Form.Label>
-              <Form.Control name="rx_percentage" id="rx_percentage" onChange={this.handleChange} type="number" placeholder="target percentage" min="0"/>
+              <Form.Control name='rx_percentage' id='rx_percentage' onChange={this.handleChange} type='number' placeholder='target percentage' min='0'/>
               <Form.Label><h5>Target weight:</h5></Form.Label>
-              <Form.Control name="weight" id="weight" onChange={this.handleChange} type="number" placeholder="weight" min="0"/>
+              <Form.Control name='weight' id='weight' onChange={this.handleChange} type='number' placeholder='weight' min='0'/>
               <Form.Label><h5>Notes:</h5></Form.Label>
-              <Form.Control name="notes" id="notes" onChange={this.handleChange} type="text" placeholder="notes" />
-              <Button variant='primary' type="submit" className='create-submit'> Submit </Button>
+              <Form.Control name='notes' id='notes' onChange={this.handleChange} type='text' placeholder='notes' />
+              <Button variant='primary' type='submit' className='create-submit'> Submit </Button>
             </Form>
           </Col>
         </div>
